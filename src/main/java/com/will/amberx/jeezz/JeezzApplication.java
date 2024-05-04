aa89a77753d245425852149dd3a171311d335c75
hemavathitamilmaran <hemavathi.tamilmaran95@gmail.com>
2024-05-04 15:40:04 +0100

    initial commit with skelton

---
+++ src/main/java/com/will/amberx/jeezz/JeezzApplication.java
@@ -0,0 +1,13 @@
+package com.will.amberx.jeezz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeezzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeezzApplication.class, args);
+	}
+
+}
